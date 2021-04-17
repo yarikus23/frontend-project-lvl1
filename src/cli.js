9cc243ec76aed9ef2a117914f2cfa2f3174623e1
yarikus23 <yarikus23@gmail.com>
2021-04-17 21:23:30 +0300

    some changes function in src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import readlineSync from 'readline-sync';
 const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
 export default getUserName;
