6abbbcbb3fb39c22cf78b0c19c06dbf18c7b103b
yarikus23 <yarikus23@gmail.com>
2021-04-26 21:23:06 +0300

    modified bin/brain-even.js and src/games/even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,45 +1,5 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
 /* eslint-disable import/extensions */
-import readlineSync from 'readline-sync';
-import getUserName from '../src/cli.js';
+import execute from '../src/games/even.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = getUserName();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-const getRandomNum = () => {
-  const randomNum = Math.floor(Math.random() * 100);
-  return randomNum;
-};
-
-for (let i = 0; i < 3; i += 1) {
-  const checkNum = getRandomNum();
-  console.log(`Question: ${checkNum}`);
-  let evenNumber;
-  if (checkNum % 2 === 0) {
-    evenNumber = true;
-  } else evenNumber = false;
-  const inquiry = readlineSync.question('Your answer: ');
-  if (evenNumber === true) {
-    if (inquiry === 'yes') {
-      console.log('Correct!');
-    } else {
-      console.log(`'${inquiry}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    }
-  }
-  if (evenNumber === false) {
-    if (inquiry === 'no') {
-      console.log('Correct!');
-    } else {
-      console.log(`'${inquiry}' is wrong answer ;(. Correct answer was 'no'.`);
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    }
-  }
-  if (i === 2) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-}
+execute();
---
+++ src/games/even.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import getRandomNum from '../getRandomNum.js';
+import getDialog from '../index.js';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const getExtension = () => {
+  const array = [];
+  for (let i = 0; i < 3; i += 1) {
+    let result;
+    const checkNum = getRandomNum(100);
+    if (checkNum % 2 === 0) {
+      result = 'yes';
+    } else result = 'no';
+    const ask = `${checkNum}`;
+    array.push([ask, result]);
+  }
+  return array;
+};
+
+const execute = () => {
+  getDialog(getExtension());
+};
+
+export default execute;
