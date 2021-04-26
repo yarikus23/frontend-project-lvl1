f9dbe15c39878dcffbb5eb1b050e7ae68fa61af4
yarikus23 <yarikus23@gmail.com>
2021-04-26 19:57:01 +0300

    add src/games/calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+import getRandomNum from '../getRandomNum.js';
+import getDialog from '../index.js';
+
+console.log('What is the result of the expression?');
+const getExtension = () => {
+  const array = [];
+  for (let i = 0; i < 3; i += 1) {
+    let result;
+    const firstOperand = getRandomNum(100);
+    const secondOperand = getRandomNum(100);
+    const operations = ['+', '-', '*'];
+    const currentOperation = operations[getRandomNum(3)];
+    const ask = `${firstOperand} ${currentOperation} ${secondOperand}`;
+    if (currentOperation === '+') {
+      result = firstOperand + secondOperand;
+    }
+    if (currentOperation === '-') {
+      result = firstOperand - secondOperand;
+    }
+    if (currentOperation === '*') {
+      result = firstOperand * secondOperand;
+    }
+    array.push([ask, result]);
+  }
+  return array;
+};
+
+getDialog(getExtension());
